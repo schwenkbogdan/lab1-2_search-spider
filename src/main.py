fd8fb5ab05cbf057fc5ecf2844281d2c8c82fd1c
Admiralty Anchor <45291723+schwenkbogdan@users.noreply.github.com>
2024-09-29 11:08:40 +0700

    Update main.py
    config file

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import Crawler_module
 
 if __name__ == '__main__':
-    start_pages = ['https://history.eco']  # Стартовые страницы
-    crawler = Crawler_module.Crawler('test.db')
-    crawler.crawl(start_pages, depth=2)  # Глубина обхода — 2
+    start_pages = ['https://history.eco'
+                   ]  # Стартовые страницы, 'https://habr.com'
+    crawler = Crawler_module.Crawler('test3.db', config_file='config.ini')
+    crawler.crawl(start_pages, 1)  # Глубина обхода
