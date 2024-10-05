dfefffe99fc4858fc3e5b439d8cdb07f0ba6757d
Admiralty Anchor <45291723+schwenkbogdan@users.noreply.github.com>
2024-10-05 10:25:00 +0700

    working crawler

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import Crawler_module
 
 if __name__ == '__main__':
-    start_pages = ['https://habr.com'
-                   ]  # Стартовые страницы, 'https://history.eco''https://habr.com'
+    start_pages = ['https://history.eco'
+                   ]  # Стартовые страницы, 'http://90.189.211.2/''https://habr.com'
     crawler = Crawler_module.Crawler('test3.db', 'config.ini')
-    crawler.crawl(start_pages, 1)  # Глубина обхода
+    crawler.crawl(start_pages, 2)  # Глубина обхода
